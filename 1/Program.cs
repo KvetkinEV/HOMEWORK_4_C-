fed67f0c71002e4251b9ee5b8b1f4dcea1934ff9
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-11 20:29:36 +0300

    Programm 1

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Stepen(int chisloa, int chislob)
+            {
+                int result = 1;
+                for (int i = 1; i <= chisloa; i++)
+                {
+                    result = chislob * result;
+                }
+                
+                return result;
+            }
+
+            Console.Write("Введите число A: ");
+            int chisloA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            int chisloB = Convert.ToInt32(Console.ReadLine());
+
+            int vivod = Stepen(chisloB, chisloA);
+            Console.WriteLine("Ответ: " + vivod);
+
+            Console.ReadLine();
+
