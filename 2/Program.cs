0259f1dd2ff93ec7ff4b152d9719a421b23f3af6
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-11 20:31:15 +0300

    Programm 2

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число N =  ");
+            int chisloN = Convert.ToInt32(Console.ReadLine());
+
+            int SumCifr(int chislon)
+            {
+
+                int schetchik = Convert.ToString(chisloN).Length;
+                int advance = 0;
+                int result = 0;
+
+                for (int i = 0; i < schetchik; i++)
+                {
+                    advance = chisloN - chisloN % 10;
+                    result = result + (chisloN - advance);
+                    chisloN = chisloN / 10;
+                }
+                return result;
+            }
+
+            int sumCifr = SumCifr(chisloN);
+            Console.WriteLine("Сумма цифр в числе =  " + sumCifr);
+            Console.ReadLine();
+
